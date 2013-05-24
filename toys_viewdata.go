4c480c76811530a2bc1d79f7f79c08cfde44f87d
nvcnvn <nguyen@open-vn.org>
2013-05-24 21:56:56 +0700

    move app to diy sub folder

--- toys_viewdata.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func (c *controller) ViewData(title string) map[string]interface{} {
-	m := make(map[string]interface{})
-	m["Title"] = title
-	m["BasePath"] = func(path string) string { return c.BasePath(path) }
-	return m
-}
